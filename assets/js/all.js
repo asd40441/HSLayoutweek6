d48bf15f68b6b988cccc280aff95a7a2d928333a
asd40441 <asd40441@hotmail.com>
2021-08-27 22:28:51 +0800

    Update 2021-08-27T14:28:45.688Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,18 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
